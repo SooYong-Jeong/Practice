4a84d5e13c6e9c914331296fa4ad1e71d4509715
sooyong <wjdtndyd7@naver.com>
2021-02-17 17:53:54 +0900

    Region작업

--- practice man.py
+++ practice man.py
@@ -1,3 +1,4 @@
+#region print
 '''
 print(5)
 print(-5)
@@ -17,26 +18,38 @@ print(False)
 print(not 1>10)
 print(not 1<10)
 '''
-# name = "정수용"
-# sex = "남"
-# age = 24
-# hight = 178
-# weight = 73
-# is_adult = age >= 20
+#endregion
+#region variable
+'''
+name = "정수용"
+sex = "남"
+age = 24
+hight = 178
+weight = 73
+is_adult = age >= 20
 
-# print("내이름은" , name)
-# print("나이는" , age , "이고")
-# print("어른인가?" , is_adult)
-
-# station = "사당"
-# print(station + "행 열차가 들어오고 있습니다.")
-# station = "신도림"
-# print(station + "행 열차가 들어오고 있습니다.")
-# station = "인천공항"
-# print(station + "행 열차가 들어오고 있습니다.")
-
-# print(2**10)
-# print(100//5)
+print("내이름은" , name)
+print("나이는" , age , "이고")
+print("어른인가?" , is_adult)
+'''
+#endregion
+#region Quiz1
+'''
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+'''
+#endregion
+#region calculation
+'''
+print(2**10)
+print(100//5)
+'''
+#endregion
+#region maxminrandom
 '''
 print(abs(-1))      #절댓값
 print(pow(2, 4))    #지수
@@ -60,10 +73,15 @@ print(int(random() *10))    #0 ~ 10미만
 print(randrange(1, 46))     #1 ~ 46미만
 print(randrange(1, 45))     #1 ~ 45
 '''
+#endregion
+#region Quiz2
+'''
+from random import *
 
-# from random import *
-
-# print("오프라인 스터디 모임 날짜는 매월"+ str(randrange(4, 29)) +"일로 선정되었습니다")
+print("오프라인 스터디 모임 날짜는 매월"+ str(randrange(4, 29)) +"일로 선정되었습니다")
+'''
+#endregion
+#region sentence
 '''
 se = '가나다라마바사'
 se2 = "가나다라마바사"
@@ -76,17 +94,21 @@ print(se)
 print(se2)
 print(se3)
 '''
+#endregion
+#region slicing
+'''
+per = "980119-1118011"
 
-# per = "980119-1118011"
-
-# print("성별 : " + per[7])
-# print("생년 : " + per[0:2])         #0 ~ 2 직전까지
-# print("월 : " + per[2:4])
-# print("일 : " + per[4:6])
-# print("생년월일 : " + per[:6])      #처음부터
-# print("뒷자리 : " + per[7:])        #끝까지
-# print("뒷자리 : " + per[-7:])       #뒤부터
-
+print("성별 : " + per[7])
+print("생년 : " + per[0:2])         #0 ~ 2 직전까지
+print("월 : " + per[2:4])
+print("일 : " + per[4:6])
+print("생년월일 : " + per[:6])      #처음부터
+print("뒷자리 : " + per[7:])        #끝까지
+print("뒷자리 : " + per[-7:])       #뒤부터
+'''
+#endregion
+#region sentence function
 '''
 python = "Python is Greatn"
 print(python.lower())       #전부 소문자
@@ -101,31 +123,35 @@ print(index)
 print(python.find("java"))  #문자찾기(없다면 -1을 반환)
 print(python.count("n"))    #문자횟수
 '''
+#endregion
+#region sentence format
+'''
+print("%d" % 30)
+print("%s" % "정수용")
+print("%c" % "a")
 
-# print("%d" % 30)
-# print("%s" % "정수용")
-# print("%c" % "a")
+print("%s" % 30)
+print("%s %s" % ("하나", "둘"))
 
-# print("%s" % 30)
-# print("%s %s" % ("하나", "둘"))
+print("{}".format(20))
+print("{} {}".format(20, 10))
+print("{1} {0}".format(20, 10))
 
-# print("{}".format(20))
-# print("{} {}".format(20, 10))
-# print("{1} {0}".format(20, 10))
+print("{color} {age}".format(age = 20, color = "하나"))
 
-# print("{color} {age}".format(age = 20, color = "하나"))
+age = 20
+color = "하나"
+print(f"{color} {age}")
 
-# age = 20
-# color = "하나"
-# print(f"{color} {age}")
-
-# print("\"\"\"\"")
-# print("\\")
-# #\r = 커서를 앞으로
-# print("asdasd\rdddddddd")
-# #\b = 백스페이스
-# #\t = 탭
-
+print("\"\"\"\"")
+print("\\")
+#\r = 커서를 앞으로
+print("asdasd\rdddddddd")
+#\b = 백스페이스
+#\t = 탭
+'''
+#endregion
+#region Quiz3
 '''
 site = "https://www.naver.com"
 site_1 = site[site.index(".") + 1:site.index(".",site.index(".") + 1)]
@@ -134,7 +160,8 @@ PW = site_1[:3] + str(len(site_1)) + str(site_1.count('e')) + "!"
 print(site_1)
 print(PW)
 '''
-
+#endregion
+#region list
 '''
 subway = [0,2,3]
 print(subway.index(2))
@@ -166,7 +193,9 @@ print(num)
 num.extend(subway)
 print(num)
 '''
-
+#endregion
+#region cabinet
+'''
 cabinet = {3:"정수용", 100:"홍길동"}
 # print(cabinet[3])   #값이 없는경우 프로그램 종료
 # print(cabinet[100])
@@ -188,3 +217,6 @@ print(cabinet.items())
 
 cabinet.clear()
 print(cabinet)
+'''
+#endregion
+
