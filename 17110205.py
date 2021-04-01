eb385da56b42efffde4be1081f0b1847c0718102
sooyong <wjdtndyd7@naver.com>
2021-04-01 21:39:17 +0900

    과제완성

--- 17110205.py
+++ 17110205.py
@@ -1,17 +1,17 @@
-class TU:
+class TU:   #학생정보 클래스
     def  __init__(self):
         self.TU = 0
 
-    def Input_ID(self):
+    def Input_ID(self): #학번 입력 함수
         print("Input Your ID : ")
         self.ID = input()
 
-    def Input_Name(self):
+    def Input_Name(self):   #이름 입력 함수
         print("Input Your Name : ")
         self.Name = input()
 
-    def print_ID_name(self):
-        print(f"Your ID is {self.Id}, and your name is {self.Name}")
+    def print_ID_name(self):    #학번, 이름 출력 함수
+        print(f"Your ID is {self.ID}, and your name is {self.Name}")
 
 JSY = TU()
 
