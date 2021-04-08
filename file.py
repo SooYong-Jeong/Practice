047eb5abc18695039b9a203ddfb0428f9c4cf7a3
sooyong <wjdtndyd7@naver.com>
2021-04-08 20:02:42 +0900

    File I.O

--- file.py
+++ file.py
@@ -1,7 +1,5 @@
 '''
 f = open("test_csv.csv", "a")
-line = "name, id, pw, addr\n"
-f.write(line)
 line = "a1, a01, a02, a03\n"
 f.write(line)
 line = "b1, b01, b02, b03\n"
@@ -15,3 +13,13 @@ lines = f.readlines()
 print(lines)
 f.close()
 '''
+import csv
+
+f=open("test_csv.csv", "r")
+
+lines=csv.reader(f)
+header = next(lines)
+for line in lines:
+    print(line)
+
+f.close()
